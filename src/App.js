291f5cea1be22af18ee4f3066735223223fa011a
Martin <martinroldan889@gmail.com>
2021-06-08 16:39:58 -0300

    [ADD] Standard. Format and linter code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello World!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
