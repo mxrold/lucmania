2ea960c0ecdca593aa0063842a11116c0d3786fe
Martin <martinroldan889@gmail.com>
2021-07-13 16:39:06 -0300

    Added configuration fetch data

--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -1,6 +1,6 @@
 const FetchData = async (firstParam, secondParam, thirdParam = 'es-MX') => {
     try {
-        const URL = `https://api.themoviedb.org/3${firstParam}?api_key=${secondParam}&language=${thirdParam}`
+        const URL = `https://api.themoviedb.org/3${firstParam}?api_key=9f9133c73447f1c78b793864d3afdda5${secondParam}&language=${thirdParam}`
         const response = await fetch(URL)
         const responseJson = await response.json()
         return responseJson
